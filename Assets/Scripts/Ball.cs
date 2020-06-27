7d42eb22aef8f8ea7584abf527242da2395c019d
Vova <volodimir.partytskyi@gmail.com>
2020-06-28 00:17:06 +0300

    Add collider for ball & moving for paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    Vector2 paddleToBallVector;
+    Rigidbody2D rb;
+
+    bool hasStoped = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasStoped)
+        {
+            LaunchOnMouseClick();
+            LockBallToPaddle();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStoped = false;
+            rb.velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+
+    Vector2 paddlePos = new Vector2(0, 0);
+
+    private void Start()
+    {
+        paddlePos.y = transform.position.y;
+    }
+
+    void Update()
+    {
+        float mousePosition = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        paddlePos.x = Mathf.Clamp(mousePosition, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
