161d34ae08e9ea06bdb5e06c0240d63520afeb01
Vova <volodimir.partytskyi@gmail.com>
2020-06-28 16:28:59 +0300

    Add all objects to prefabs

--- Assets/Scripts/BottomWall.cs
+++ Assets/Scripts/BottomWall.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoseCollider : MonoBehaviour
+public class BottomWall : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
