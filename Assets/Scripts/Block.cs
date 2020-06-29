afe0b26fcb47e35821707d7aea5c778bc312043c
Vova <volodimir.partytskyi@gmail.com>
2020-06-29 21:44:30 +0300

    Add block damage level

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,30 +1,73 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] int maxHits = 1;
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
 
     Level level;
     GameStatus gameStatus;
+    int timesHit = 0;
 
     private void Start()
     {
-        level = FindObjectOfType<Level>();
         gameStatus = FindObjectOfType<GameStatus>();
 
-        level.CountBreakableBlocks();
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (CompareTag("Breakable")) level.CountBlocks();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (CompareTag("Breakable")) HandleHit();
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            PlaySoundEffect();
+            AddToScore();
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+    }
+
+    private void PlaySoundEffect()
+    {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        gameStatus.AddToScore();
+    }
+
+    private void DestroyBlock()
+    {
         level.BlockDestroyed();
         Destroy(gameObject);
         TriggerSparklesVFX();
+    }
+
+    private void AddToScore()
+    {
+        gameStatus.AddToScore();
     }
 
     private void TriggerSparklesVFX()
