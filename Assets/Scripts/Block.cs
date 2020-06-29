cf4e66b58f07f5161fe76b63669091685d6e1df4
Vova <volodimir.partytskyi@gmail.com>
2020-06-29 20:29:46 +0300

    Add explosion effect to block after destroy

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
 
     Level level;
     GameStatus gameStatus;
@@ -23,5 +24,12 @@ public class Block : MonoBehaviour
         gameStatus.AddToScore();
         level.BlockDestroyed();
         Destroy(gameObject);
+        TriggerSparklesVFX();
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
